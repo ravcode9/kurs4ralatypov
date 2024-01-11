da4cb857fffe5b7777b1f2eabc1ae296f1fec5ce
ravcode9 <rlatypov2106@gmail.com>
2024-01-11 16:02:53 +0500

    new func extract_salary

--- src/vacancies.py
+++ src/vacancies.py
@@ -19,3 +19,27 @@ class Vacancy:
 
         self.requirements = kwargs.get("snippet", {}).get("requirement", "") or kwargs.get("work", "")
         self.extra_data = kwargs  # Сохраняем дополнительные данные
+
+    def extract_salary(self) -> str:
+        """
+        Извлекает информацию о заработной плате в формате строки.
+        """
+        salary_from = self.salary.get("from")
+        salary_to = self.salary.get("to")
+
+        if salary_from is not None and salary_to is not None:
+            if salary_from == salary_to:
+                return f"от {salary_from}"
+            else:
+                return f"от {salary_from} до {salary_to}"
+        elif salary_from is not None:
+            return f"от {salary_from}"
+        elif salary_to is not None:
+            return f"до {salary_to}"
+        else:
+            # В случае SuperJob используем поле "payment_from"
+            salary_from_sj = self.extra_data.get("payment_from", None)
+            if salary_from_sj is not None:
+                return f"от {salary_from_sj}"
+            else:
+                return "зарплата не указана"
