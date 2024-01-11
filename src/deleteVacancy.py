ae4ad29f2abd1ebe6767a82f781b0590a26ec92f
ravcode9 <rlatypov2106@gmail.com>
2024-01-11 16:35:25 +0500

    deleteVacancy fix

--- src/deleteVacancy.py
+++ src/deleteVacancy.py
@@ -5,11 +5,12 @@ if __name__ == "__main__":
     json_saver = JSONSaver()
 
     # Загрузка существующих вакансий из JSON-файла
+    json_saver.load_from_file()
     all_vacancies = json_saver.vacancies
 
     # Определение вакансии, которую нужно удалить на основе её названия и ссылки
-    vacancy_to_delete_title = "вставь название вакансии"
-    vacancy_to_delete_link = "вставь ссылку вакансии"
+    vacancy_to_delete_title = "Программист-разработчик Python"
+    vacancy_to_delete_link = "https://www.superjob.ru/vakansii/programmist-razrabotchik-python-42601580.html"
 
     # Поиск вакансии в списке всех вакансий
     vacancy_to_delete = next((v for v in all_vacancies if v.get("title") == vacancy_to_delete_title and v.get("link") == vacancy_to_delete_link), None)
@@ -20,5 +21,6 @@ if __name__ == "__main__":
 
         # Сохранение обновлённого списка вакансий в JSON-файл
         json_saver.save_to_file()
+        print(f"Вакансия '{vacancy_to_delete_title}' по ссылке {vacancy_to_delete_link} удалена.")
     else:
-        print("Вакансия не найдена.")
+        print(f"Вакансия '{vacancy_to_delete_title}' по ссылке {vacancy_to_delete_link} не найдена.")
