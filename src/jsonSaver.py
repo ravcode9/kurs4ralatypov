910365afdb75b10db73f4ff54172ee4018250ffb
ravcode9 <rlatypov2106@gmail.com>
2024-01-11 16:14:38 +0500

    new funcs

--- src/jsonSaver.py
+++ src/jsonSaver.py
@@ -60,3 +60,58 @@ class JSONSaver(AbstractVacancySaver):
         else:
             print("Файл с данными пуст или отсутствует.")
             self.vacancies = []
+
+    def add_vacancy(self, vacancy: Vacancy) -> None:
+        """Добавляет вакансию в хранилище."""
+        self.load_from_file()
+        existing_titles_and_links = [(v.get("title"), v.get("link")) for v in self.vacancies]
+        if (vacancy.title, vacancy.link) not in existing_titles_and_links:
+            self.vacancies.append({
+                "title": vacancy.title,
+                "link": vacancy.link,
+                "salary": vacancy.extract_salary(),
+                "requirements": vacancy.get_requirements(),
+            })
+            self.vacancies.sort(key=lambda x: x['title'])  # Сортировка по алфавиту названия
+            self.save_to_file()
+        else:
+            print(f"Вакансия '{vacancy.title}' по ссылке {vacancy.link} уже существует.")
+
+    def get_vacancies_by_salary(self, min_salary: float) -> List[Vacancy]:
+        """Возвращает вакансии с зарплатой не ниже указанной."""
+        return [Vacancy(**v) for v in self.vacancies if
+                float(Vacancy(**v).extract_salary().replace(" ", "")) >= min_salary]
+
+    def delete_vacancy(self, vacancy: Union[Dict[str, str], Vacancy]) -> None:
+        """Удаляет вакансию из хранилища."""
+        if isinstance(vacancy, dict):
+            self.vacancies = [v for v in self.vacancies if
+                              v.get("title") != vacancy.get("title") or v.get("link") != vacancy.get("link")]
+            self.save_to_file()
+        else:
+            print("Неверный формат, ожидается словарь.")
+
+    def save_to_file(self):
+        """Сохраняет данные в файл."""
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            json.dump(self.vacancies, file, ensure_ascii=False, indent=2)
+
+    def filter_vacancies(self, *vacancies: List[Dict[str, str]], filter_words: List[str]) -> List[Dict[str, str]]:
+        """Фильтрует вакансии по ключевым словам."""
+        filtered_vacancies = []
+        for vacancy in vacancies:
+            filtered_vacancies.extend([v for v in vacancy if self.word_match(v, filter_words)])
+        return filtered_vacancies
+
+    def word_match(self, vacancy: Union[Vacancy, Dict[str, str]], filter_words: List[str]) -> bool:
+        """Проверяет, соответствует ли вакансия ключевым словам."""
+        if isinstance(vacancy, Vacancy):
+            vacancy_data = vacancy.extra_data
+        else:
+            vacancy_data = vacancy
+
+        return any(word.lower() in str(vacancy_data).lower() for word in filter_words)
+
+    def sort_vacancies(self, vacancies: List[Union[Dict[str, str], Vacancy]]) -> List[Union[Dict[str, str], Vacancy]]:
+        """Сортирует вакансии по заработной плате."""
+        return sorted(vacancies, key=lambda v: Vacancy(**v).extract_salary() if isinstance(v, dict) else v.extract_salary(), reverse=True)
